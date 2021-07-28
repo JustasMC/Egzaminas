c599642e5e6278ddad910446dfe5764d4bd9e822
Your Name <justasmc91@gmail.com>
2021-07-28 11:53:53 +0300

    vartotojai

--- server/server.js
+++ server/server.js
@@ -17,6 +17,25 @@ app.use(bodyParser.json());
 const { User } = require('./models/user');
 
 
+// const port = process.env.PORT || 3001;
+// app.listen(port,()=>{
+//     console.log(`Started on port ${port}`)
+// })
+
+/// ROUTES
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc);
+    });
+})
+
+
+
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`Started on port ${port}`)
